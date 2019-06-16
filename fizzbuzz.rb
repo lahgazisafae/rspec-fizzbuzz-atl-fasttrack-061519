312400ec6c5a63e7a6ff303092dac94c857a8b90
Safae Lahgazi Alaoui <safae.alaoui@flatironschool.com>
2019-06-16 15:46:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    "Fizz"
+  if num % 3 &&  5 == 0
   elsif num % 5 == 0
     "Buzz"
-  elsif num % 3 &&  5 == 0
+  elsif num % 3 == 0
+    "Fizz"
     "Fizzbuzz"
   else
     nil
